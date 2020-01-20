1c5958cd71f7796d7eb9d20379449bd7b32251a5
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-01-20 23:30:30 +0500

    Finding the global pressed key

--- StopWatch/MainWindow.xaml.cs
+++ StopWatch/MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Shapes;
 using System.Diagnostics;
 using System.Windows.Threading;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
 
 namespace StopWatch
 {
@@ -25,11 +27,30 @@ namespace StopWatch
         private int idCount;
         private string currentTime;
 
+        public string documentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private const int HOTKEY_ID = 9000;
+
+        //Modifiers:
+        private const uint MOD_NONE = 0x0000; //(none)
+        private const uint MOD_ALT = 0x0001; //ALT
+        private const uint MOD_CONTROL = 0x0002; //CTRL
+        private const uint MOD_SHIFT = 0x0004; //SHIFT
+        private const uint MOD_WIN = 0x0008; //WINDOWS
+        //CAPS LOCK:
+        private const uint VK_CAPITAL = 0x14;
         public MainWindow()
         {
             InitializeComponent();
             Timer_Loader();
             TimerStage = eTimerStage.Paussed;
+            Path_label.Content = $"{documentPath}";
         }
 
         void Timer_Loader()
@@ -69,9 +90,8 @@ namespace StopWatch
         {
             Items item = new Items(++idCount, currentTime);
             ListItems1.Items.Add(item);
+            btSave.IsEnabled = true;
         }
-
-
 
         private void btStart_Click(object sender, RoutedEventArgs e)
         {
@@ -98,20 +118,65 @@ namespace StopWatch
 
         private void btSave_Click(object sender, RoutedEventArgs e)
         {
-
             // Create a string array with the lines of text
-            // Set a variable to the Documents path.
-            string documentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
             // Append text to an existing file named "WriteLines.txt".
             using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(documentPath, "WriteLines.txt")))
-                foreach (Items item in ListItems1.Items)
-                {
-                    outputFile.WriteLine($"{item.Id + "." + " " + item.Time}");
-                }
+           
+            foreach (Items item in ListItems1.Items)
+            {
+                outputFile.WriteLine($"{item.Id + "." + " " + item.Time}");
+            }
         }
 
-        // как винда понимает какая кнопка нажата через user32 библиотеку
+        private void Path_label3_KeyDown(object sender, KeyEventArgs e)
+        {
+            {
+                if (e.KeyboardDevice.Modifiers == ModifierKeys.Shift && e.Key == Key.F1)
+                    Path_label3.Content = MessageBox.Show("HELLO");
+            }
+        }
+
+        private IntPtr _windowHandle;
+        private HwndSource _source;
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            _windowHandle = new WindowInteropHelper(this).Handle;
+            _source = HwndSource.FromHwnd(_windowHandle);
+            _source.AddHook(HwndHook);
+
+            RegisterHotKey(_windowHandle, HOTKEY_ID, MOD_CONTROL, VK_CAPITAL); //CTRL + CAPS_LOCK
+        }
+
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_HOTKEY = 0x0312;
+            switch (msg)
+            {
+                case WM_HOTKEY:
+                    switch (wParam.ToInt32())
+                    {
+                        case HOTKEY_ID:
+                            int vkey = (((int)lParam >> 16) & 0xFFFF);
+                            if (vkey == VK_CAPITAL)
+                            {
+                                Path_label3.Content += "CapsLock was pressed" + Environment.NewLine;
+                            }
+                            handled = true;
+                            break;
+                    }
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _source.RemoveHook(HwndHook);
+            UnregisterHotKey(_windowHandle, HOTKEY_ID);
+            base.OnClosed(e);
+        }
 
     }
 
@@ -126,6 +191,7 @@ namespace StopWatch
             this.Time = Time;
         }
     }
+
 
 
 }
