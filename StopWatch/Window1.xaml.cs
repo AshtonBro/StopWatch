12f81c1be5e4a7712e3fe4eada75fea33610e14a
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-02-03 22:27:54 +0500

    Need to create a binding

---
+++ StopWatch/Window1.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StopWatch
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public void winCenter (Window _owner)
+        {
+           
+            //  This call is required by the designer.
+            InitializeComponent();
+            this.Owner = _owner;
+            //  Add any initialization after the InitializeComponent() call.
+        }
+    }
+    
+}
